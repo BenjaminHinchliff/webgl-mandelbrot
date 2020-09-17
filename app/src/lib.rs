0d89b473fe84ebb08710c2b1459b0edc30cc9904
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-09-16 22:24:34 -0700

    add panic handling and color for console log

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,7 +3,9 @@ use log::{Level, info};
 
 #[wasm_bindgen(start)]
 pub fn run() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init_with_level(Level::Debug).expect("failed to init logging");
 
     info!("Hello world!");
+    panic!("hello panic!");
 }
