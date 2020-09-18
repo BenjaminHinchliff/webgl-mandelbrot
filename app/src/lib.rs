96838ef434aeb24352d0d677b85c4614bc41bd96
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-09-18 13:55:03 -0700

    add basic code

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,11 +1,15 @@
 use wasm_bindgen::prelude::*;
 use log::{Level, info};
 
-#[wasm_bindgen(start)]
-pub fn run() {
+#[wasm_bindgen]
+pub fn render_mandelbrot() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init_with_level(Level::Debug).expect("failed to init logging");
 
-    info!("Hello world!");
-    panic!("hello panic!");
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    let canvas = document.get_element_by_id("screen").expect("element with #screen must exist");
+
+    info!("{:#?}", canvas);
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import('../app/pkg');
+async function main() {
+    const app = await import('../app/pkg');
+    app.render_mandelbrot();
+}
+
+main();
